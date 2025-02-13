f730d5eb3626ec6f479cb1a417b9bd285f628541
zy <1036320830@qq.com>
2025-02-13 17:17:29 +0800

    1

--- src/components/MyCalendar/index.jsx
+++ src/components/MyCalendar/index.jsx
@@ -2,39 +2,43 @@ import './index.less'
 import { View } from '@tarojs/components'
 import moment from 'moment'
 import { ArrowLeft, ArrowRight } from '@nutui/icons-react-taro'
-import { Space, Button } from '@nutui/nutui-react-taro'
+import { Space, Button, Image } from '@nutui/nutui-react-taro'
 import { useState } from 'react'
+import src from '../../images/JBPhoto/luanchun.jpg'
 
 const daysOfWeek = ['日', '一', '二', '三', '四', '五', '六'];
 export default function MyCalendar() {
-
-  const [dateObj, setDateObj] = useState({
-    currentDate: moment(),
-    currentDayofMonth: moment().date(),
-    daysInMonth: moment().daysInMonth(),
-    firstDayOfWeek: moment().startOf('month').day(),
-    YearAndMonth: moment().format('YYYY年MM月')
-  })
-
   // const currentDate = moment();
   // const daysInMonth = currentDate.daysInMonth(); // 获取当前月份的天数
   // const firstDayOfWeek = currentDate.startOf('month').day(); // 获取当前月份第一天是星期几
   // const currentDayofMonth = moment().date()
 
+  const [currentDate, setCurrentDate] = useState(moment());
+  const daysInMonth = currentDate.daysInMonth(); // 获取当前月份的天数
+  const firstDayOfWeek = currentDate.startOf('month').day(); // 获取当前月份第一天是星期几
+  const currentDayOfMonth = moment().date(); // 获取当前是本月的第几天
+
   const onAClick = (type) => {
     if (type == 'left') {
-      
+      setCurrentDate(currentDate.clone().subtract(1, 'month'));
     }
     if (type == 'right') {
-
+      setCurrentDate(currentDate.clone().add(1, 'month'));
     }
   }
-
+  const renderDayNow = (index) => {
+    const now = moment()
+    if (currentDate.format('YYYY-MM') == now.format('YYYY-MM') && currentDayOfMonth == index + 1) {
+      return 'day-now'
+    }
+    return ''
+  }
+  // const src = '../../images/JBPhoto/luanchun.jpg'
   return (<View className="calendar">
     <View className={'calendar-header'}>
       <Space align={'center'} justify={'center'}>
         <ArrowLeft size={14} onClick={() => onAClick('left')} />
-        <View>{dateObj.YearAndMonth}</View>
+        <View>{currentDate.format('YYYY年MM月')}</View>
         <ArrowRight size={14} onClick={() => onAClick('right')} />
       </Space>
 
@@ -48,14 +52,16 @@ export default function MyCalendar() {
         ))}
       </View>
       <View className="days">
-        {[...Array(dateObj.firstDayOfWeek)].map((_, index) => (
+        {[...Array(firstDayOfWeek)].map((_, index) => (
           <View key={index} className="empty-day"></View>
         ))}
-        {[...Array(dateObj.daysInMonth)].map((_, index) => (
+        {[...Array(daysInMonth)].map((_, index) => (
           <View className={`day-item `}>
-            <View key={index} className={`day-num ${dateObj.currentDayofMonth == index + 1 ? 'day-now' : ''}`}>
-              {index + 1}
-            </View>
+            {/* <View key={index} className={`day-num ${renderDayNow(index)}`}> */}
+            {/* {index + 1} */}
+
+            {/* </View> */}
+            <Image src={src} height={80} />
           </View>
 
         ))}
